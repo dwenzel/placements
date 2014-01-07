3eb7debd1cfa164c903c173b5e7918c4dd30f94d
Dirk Wenzel <der-wenzel@gmx.de>
2014-01-07 17:26:10 +0100

    new field showSearchField added to plugin, localization and BE Hook adapted

--- Classes/Hooks/T3libBefunc.php
+++ Classes/Hooks/T3libBefunc.php
@@ -43,7 +43,7 @@ class T3libBefunc {
 	 */
 	public $removedFieldsInPositionListView = array(
 			'sDEF' => '',
-			'constraints' => 'showWorkingHours,showSectors,showPositionTypes,showConjunctionSelector,showCategorySelector,clientsOrganizationsOnly',
+			'constraints' => 'showWorkingHours,showSectors,showPositionTypes,showConjunctionSelector,showCategorySelector,clientsOrganizationsOnly,showClientsPositionsCheckbox,showSearchField',
 		);
 
 	/**
@@ -63,7 +63,7 @@ class T3libBefunc {
 	 */
 	public $removedFieldsInPositionSearchFormView = array(
 			'sDEF' => 'orderBy,orderDirection',
-			'constraints' => 'workingHours,sectors,positionTypes,showWorkingHours,showSectors,showPositionTypes,constraintsConjunction,showConjunctionSelector,showCategorySelector,categories,categoryConjunction,clientsOrganizationsOnly,clientsPositionsOnly,limit,showClientsPositionsCheckbox',
+			'constraints' => 'workingHours,sectors,positionTypes,showWorkingHours,showSectors,showPositionTypes,constraintsConjunction,showConjunctionSelector,showCategorySelector,categories,categoryConjunction,clientsOrganizationsOnly,clientsPositionsOnly,limit,showClientsPositionsCheckbox,showSearchField',
 		);
 
 	/**
@@ -73,7 +73,7 @@ class T3libBefunc {
 	 */
 	public $removedFieldsInPositionExtendedSearchFormView = array(
 			'sDEF' => 'orderBy,orderDirection',
-			'constraints' => 'limit,clientsOrganizationsOnly',
+			'constraints' => 'limit,clientsOrganizationsOnly,showSearchField',
 		);
 
 	/**
@@ -83,7 +83,7 @@ class T3libBefunc {
 	 */
 	public $removedFieldsInPositionSearchResultView = array(
 			'sDEF' => '',
-			'constraints' => 'workingHours,sectors,positionTypes,showWorkingHours,showSectors,showPositionTypes,constraintsConjunction,showConjunctionSelector,categories,showCategorySelector,categoryConjunction,clientsOrganizationsOnly,clientsPositionsOnly,showClientsPositionsCheckbox',
+			'constraints' => 'workingHours,sectors,positionTypes,showWorkingHours,showSectors,showPositionTypes,constraintsConjunction,showConjunctionSelector,categories,showCategorySelector,categoryConjunction,clientsOrganizationsOnly,clientsPositionsOnly,showClientsPositionsCheckbox,showSearchField',
 		);
 
 	/**
@@ -93,7 +93,7 @@ class T3libBefunc {
 	 */
 	public $removedFieldsInUserListView = array(
 			'sDEF' => '',
-			'constraints' => 'workingHours,sectors,positionTypes,showWorkingHours,showSectors,showPositionTypes,constraintsConjunction,showConjunctionSelector,showCategorySelector,clientsOrganizationsOnly,clientsPositionsOnly,showClientsPositionsCheckbox',
+			'constraints' => 'workingHours,sectors,positionTypes,showWorkingHours,showSectors,showPositionTypes,constraintsConjunction,showConjunctionSelector,showCategorySelector,clientsOrganizationsOnly,clientsPositionsOnly,showClientsPositionsCheckbox,showSearchField',
 		);
 
 	/**
@@ -103,7 +103,7 @@ class T3libBefunc {
 	 */
 	public $removedFieldsInApplicationListView = array(
 			'sDEF' => '',
-			'constraints' => 'workingHours,sectors,positionTypes,showWorkingHours,showSectors,showPositionTypes,constraintsConjunction,showConjunctionSelector,showCategorySelector,clientsOrganizationsOnly,clientsPositionsOnly,showClientsPositionsCheckbox',
+			'constraints' => 'workingHours,sectors,positionTypes,showWorkingHours,showSectors,showPositionTypes,constraintsConjunction,showConjunctionSelector,showCategorySelector,clientsOrganizationsOnly,clientsPositionsOnly,showClientsPositionsCheckbox,showSearchField',
 		);
 
 	/**
@@ -113,7 +113,7 @@ class T3libBefunc {
 	 */
 	public $removedFieldsInOrganizationListView = array(
 			'sDEF' => '',
-			'constraints' => 'workingHours,positionTypes,showWorkingHours,showPositionTypes,constraintsConjunction,showConjunctionSelector,showClientsPositionsCheckbox,clientsPositionsOnly',
+			'constraints' => 'workingHours,positionTypes,showWorkingHours,showPositionTypes,constraintsConjunction,showConjunctionSelector,showClientsPositionsCheckbox,clientsPositionsOnly,showSearchField',
 		);
 
 
