4f78e77d365741173c5ae22c073e86240b1a1f35
Dirk Wenzel <der-wenzel@gmx.de>
2014-01-08 17:03:34 +0100

    exclude oms from concatenation

--- Resources/Private/TypoScript/javascript.ts
+++ Resources/Private/TypoScript/javascript.ts
@@ -1,6 +1,9 @@
 page.includeJSFooter {
 		oms = EXT:placements/Resources/Public/Js/Lib/oms.min.js
-		oms.disableCompression = 1
+		oms {
+			disableCompression = 1
+			excludeFromConcatenation = 1
+		}
 		placements = EXT:placements/Resources/Public/Js/placements.js
 }
 
