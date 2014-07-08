2a80de4477b39ad271489c0043f286df6ac6a3ea
Dirk Wenzel <der-wenzel@gmx.de>
2014-07-08 17:02:13 +0200

    building query constraints for clients implemented

--- Classes/Domain/Repository/OrganizationRepository.php
+++ Classes/Domain/Repository/OrganizationRepository.php
@@ -44,6 +44,20 @@ class OrganizationRepository extends AbstractDemandedRepository {
 	protected function createConstraintsFromDemand (\TYPO3\CMS\Extbase\Persistence\QueryInterface $query, \Webfox\Placements\Domain\Model\Dto\DemandInterface $demand) {
 		//@todo implement constraints
 		$constraints = array();
+		
+		/**
+		 * we have to build an empty constraint
+		 * in order to prevent organizations without a client from beeing
+		 * shown when current user has no client
+		 */
+		if ($demand->getClients() != '') {
+			$clients = explode(',', $demand->getClients());
+			foreach($clients as $client) {
+				$constraints[] = $query->equals('client.uid', $client);
+			}
+		} else {
+			$constraints[] = $query->equals('client', '');
+		}
 		return $constraints;
 	}
 
