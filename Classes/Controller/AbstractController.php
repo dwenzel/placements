8db64322888c09f75f93d09ea66ac990a02c635d
Dirk Wenzel <der-wenzel@gmx.de>
2014-07-18 17:07:17 +0200

    code cleanup

--- Classes/Controller/AbstractController.php
+++ Classes/Controller/AbstractController.php
@@ -264,34 +264,6 @@ class AbstractController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControl
 	public function translate($key, $extension='placements', $arguments=NULL) {
 		return \TYPO3\CMS\Extbase\Utility\LocalizationUtility::translate($key, $extension, $arguments);
 	}
-	/**
-	 * @return void
-	 * @override \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
-	 */
-/*	protected function callActionMethod() {
-		try {
-			parent::callActionMethod();
-		}
-		catch(\Exception $exception) {
-			// This enables you to trigger the call of TYPO3s page-not-found handler by throwing \TYPO3\CMS\Core\Error\Http\PageNotFoundException
-			if ($exception instanceof \TYPO3\CMS\Core\Error\Http\PageNotFoundException) {
-				$GLOBALS['TSFE']->pageNotFoundAndExit($this->entityNotFoundMessage);
-			}
-
-			// $GLOBALS['TSFE']->pageNotFoundAndExit has not been called, so the exception is of unknown type.
-			\VendorName\ExtensionName\Logger\ExceptionLogger::log($exception, $this->request->getControllerExtensionKey(), \VendorName\ExtensionName\Logger\ExceptionLogger::SEVERITY_FATAL_ERROR);
-			// If the plugin is configured to do so, we call the page-unavailable handler.
-			if (isset($this->settings['usePageUnavailableHandler']) && $this->settings['usePageUnavailableHandler']) {
-				$GLOBALS['TSFE']->pageUnavailableAndExit($this->unknownErrorMessage, 'HTTP/1.1 500 Internal Server Error');
-			}
-			// Else we append the error message to the response. This causes the error message to be displayed inside the normal page layout. WARNING: the plugins output may gets cached.
-			if ($this->response instanceof \TYPO3\CMS\Extbase\Mvc\Web\Response) {
-				$this->response->setStatus(500);
-			}
-			$this->response->appendContent($this->unknownErrorMessage);
-		}
-	}
-	*/
 }
 ?>
 
