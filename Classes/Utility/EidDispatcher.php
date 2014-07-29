96020a96629dd6a1b330df3109724a468bd5066c
Dirk Wenzel <der-wenzel@gmx.de>
2014-07-29 11:54:15 +0200

    copyright notice adapted, closing php tags removed, code formatting, closes #83

--- Classes/Utility/EidDispatcher.php
+++ Classes/Utility/EidDispatcher.php
@@ -1,33 +1,24 @@
 <?php
- 
-/** *************************************************************
+/**
+ * This file is part of the TYPO3 CMS project.
  *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */ 
+ 
+/**
  * Extbase Dispatcher for Ajax Calls TYPO3 6.1 namespaces
  *
  * IMPORTANT Use this script only in Extensions with namespaces
  *
- * Klaus Heuer <klaus.heuer@t3-developer.com>
- * Dirk Wenzel <wenzel@webfox01.de>
- *
- * This script is part of the TYPO3 project. The TYPO3 project is
- * free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * The GNU General Public License can be found at
- * http://www.gnu.org/copyleft/gpl.html.
- *
- * This script is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * This copyright notice MUST APPEAR in all copies of the script!
- */
- 
-/**
- * Eid Dispatcher 
+ * @author Klaus Heuer <klaus.heuer@t3-developer.com>
+ * @author Dirk Wenzel <wenzel@webfox01.de>
  * originally written by Klaus Heuer
  * adapted by Dirk Wenzel
  *
@@ -149,4 +140,3 @@ $dispatcher = $objectManager->get('TYPO3\CMS\Extbase\Mvc\Dispatcher');
 $dispatcher->dispatch($request, $response);
  
 echo $response->getContent();
-?>
