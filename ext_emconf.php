c2888e2b73812f89978549f22cd32a929c29bdb3
Dirk Wenzel <wenzel@cps-it.de>
2015-10-18 10:56:41 +0200

    [TASK] set TYPO3 version to 7.5.99. Debug view helper removed

--- ext_emconf.php
+++ ext_emconf.php
@@ -30,7 +30,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.11.1',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-6.2.99',
+			'typo3' => '6.2.0-7.5.99',
 		),
 		'conflicts' => array(
 		),
